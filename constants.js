b03485b67cf0746e67a67912792f357f48b83516
Vidhi Tiwari <tiwarividhi1947@gmail.com>
2024-07-18 23:46:21 +0530

    keeping method type in constants
    Signed-off-by: Vidhi Tiwari <tiwarividhi1947@gmail.com>

--- constants.js
+++ constants.js
@@ -1,3 +1,4 @@
  const baseUrl="https://api.openweathermap.org/data/2.5/weather"
 const appKey="292cea1892c30e0a2450d85c177c448e"
-export {baseUrl,appKey}
+const GET="GET"
+export {baseUrl,appKey,GET}
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import { baseUrl,appKey } from "./constants.js";
+import { baseUrl,appKey ,GET} from "./constants.js";
 import { callApi } from "./app.js";
 
 async function getData(baseUrl,city,appKey) {
     const url =
   `${baseUrl}?q=${city}&appid=${appKey}`;
-  let response = await callApi(url,"GET");
+  let response = await callApi(url,GET);
   console.log(response);
 }
 getData(baseUrl,"Pune",appKey);
