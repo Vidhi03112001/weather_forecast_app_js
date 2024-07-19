0386598a3b54035d021ed7defd5c2d667e6404bc
Vidhi Tiwari <tiwarividhi1947@gmail.com>
2024-07-19 23:28:52 +0530

    Keeping showWeatherData function in seperate file
    Signed-off-by: Vidhi Tiwari <tiwarividhi1947@gmail.com>

--- app/app.js
+++ app/app.js
@@ -1,3 +1,19 @@
+import { getDateTime, getTimezone } from "./utility.js";
+
+let cityInput = document.getElementById("city-input");
+let cityMood = document.querySelector(".city-mood");
+let cityName = document.querySelector(".city-name");
+let cityTemp = document.querySelector(".city-temp");
+let cityDesc = document.querySelector(".city-desc");
+let cityTime = document.querySelector(".time");
+let cityDate = document.querySelector(".date");
+let timezone = document.querySelector(".timezone");
+let windSpeed = document.querySelector("#wind-speed");
+let humidity = document.querySelector("#humidity");
+let pressure = document.querySelector("#pressure");
+let sunrise = document.querySelector("#sunrise");
+let sunset = document.querySelector("#sunset");
+
 export const callApi = async (url, methodType) => {
   try {
     let response = await fetch(url, {
@@ -13,4 +29,21 @@ export const callApi = async (url, methodType) => {
     console.log(error.message);
     return {};
   }
+};
+
+export const showWeatherData = (response) => {
+  console.log(response);
+  cityMood.innerText = response["weather"][0]["main"];
+  cityName.innerText = response["name"];
+  cityTemp.innerText = `${response["main"]["temp"]}°C`;
+  cityDesc.innerText = response["weather"][0]["description"];
+  cityTime.innerText = getDateTime(response["dt"]).time;
+  cityDate.innerText = getDateTime(response["dt"]).date;
+  timezone.innerText = getTimezone(response["timezone"]);
+  windSpeed.innerText = `${response["wind"]["speed"]}m/s`;
+  humidity.innerText = `${response["main"]["humidity"]}%`;
+  pressure.innerText = `${response["main"]["pressure"]}mmHg`;
+  sunrise.innerText = getDateTime(response["sys"]["sunrise"]).time;
+  sunset.innerText = getDateTime(response["sys"]["sunset"]).time;
+  cityInput.value = "";
 };
--- index.js
+++ index.js
@@ -2,42 +2,17 @@ import { GET } from "./app/constants.js";
 import { callApi } from "./app/app.js";
 import { getUrl } from "./app/utility.js";
 import { metric } from "./app/constants.js";
-import { getDateTime, getTimezone } from "./app/utility.js";
+import {showWeatherData } from "./app/app.js"
 
 let cityInput = document.getElementById("city-input");
 let responseElement = document.querySelector(".response");
-let cityMood = document.querySelector(".city-mood");
-let cityName = document.querySelector(".city-name");
-let cityTemp = document.querySelector(".city-temp");
-let cityDesc = document.querySelector(".city-desc");
-let cityTime = document.querySelector(".time");
-let cityDate = document.querySelector(".date");
-let timezone = document.querySelector(".timezone");
-let windSpeed = document.querySelector("#wind-speed");
-let humidity = document.querySelector("#humidity");
-let pressure = document.querySelector("#pressure");
-let sunrise = document.querySelector("#sunrise");
-let sunset = document.querySelector("#sunset");
 
 cityInput.addEventListener("keypress", async (event) => {
   if (event.key === "Enter") {
     event.preventDefault();
     responseElement.style.display = "block";
     let response = await getWeatherData(cityInput.value);
-    console.log(response);
-    cityMood.innerText = response["weather"][0]["main"];
-    cityName.innerText = response["name"];
-    cityTemp.innerText = `${response["main"]["temp"]}°C`;
-    cityDesc.innerText = response["weather"][0]["description"];
-    cityTime.innerText = getDateTime(response["dt"]).time;
-    cityDate.innerText = getDateTime(response["dt"]).date;
-    timezone.innerText = getTimezone(response["timezone"]);
-    windSpeed.innerText = `${response["wind"]["speed"]}m/s`;
-    humidity.innerText = `${response["main"]["humidity"]}%`;
-    pressure.innerText = `${response["main"]["pressure"]}mmHg`;
-    sunrise.innerText = getDateTime(response["sys"]["sunrise"]).time;
-    sunset.innerText = getDateTime(response["sys"]["sunset"]).time;
-    cityInput.value = "";
+    showWeatherData(response)
   }
 });
 async function getWeatherData(city) {
