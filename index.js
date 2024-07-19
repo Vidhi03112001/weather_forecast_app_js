f0ca5913bab65e4092cba382441d14f7c9186169
Vidhi Tiwari <tiwarividhi1947@gmail.com>
2024-07-19 15:28:21 +0530

    Changing the name of the function to getWeatherData
    Signed-off-by: Vidhi Tiwari <tiwarividhi1947@gmail.com>

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import { GET } from "./constants.js";
 import { callApi } from "./app.js";
 import { getUrl } from "./utility.js";
 
-async function getData(city) {
+async function getWeatherData(city) {
   let url = getUrl(city);
   let response = await callApi(url, GET);
   console.log(response);
 }
-getData("Pune");
+getWeatherData("Pune");
