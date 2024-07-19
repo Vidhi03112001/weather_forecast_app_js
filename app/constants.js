548551f47233ef0524ac04c8db7b43731b41ece5
Vidhi Tiwari <tiwarividhi1947@gmail.com>
2024-07-19 22:04:58 +0530

    Adding metric parameter in conatants
    Signed-off-by: Vidhi Tiwari <tiwarividhi1947@gmail.com>

--- app/constants.js
+++ app/constants.js
@@ -1,4 +1,5 @@
 const baseUrl = "https://api.openweathermap.org/data/2.5/weather";
 const appKey = "292cea1892c30e0a2450d85c177c448e";
+const metric = "metric";
 const GET = "GET";
-export { baseUrl, appKey, GET };
+export { baseUrl, appKey, GET, metric };
