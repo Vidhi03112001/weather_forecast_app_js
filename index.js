862b723c8a593e418cc28c3753cc229a6d67d482
Vidhi Tiwari <tiwarividhi1947@gmail.com>
2024-07-20 11:53:17 +0530

    Validating input before weather api call
    Signed-off-by: Vidhi Tiwari <tiwarividhi1947@gmail.com>

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-import { showWeatherData ,showError,getWeatherData} from "./app/app.js";
+import {
+  showWeatherData,
+  showError,
+  getWeatherData,
+  validateInput,
+} from "./app/app.js";
 import { cityInput } from "./elements.js";
 cityInput.addEventListener("keypress", async (event) => {
   if (event.key === "Enter") {
     event.preventDefault();
-    let response = await getWeatherData(cityInput.value);
-    if (response.error === null) {
-      showWeatherData(response.data);
+    if (validateInput(cityInput.value)) {
+      let response = await getWeatherData(cityInput.value);
+      if (response.error === null) {
+        showWeatherData(response.data);
+      } else {
+        showError(response.error);
+      }
     } else {
-      showError(response.error)
+      showError("Invalid Input")
     }
+
     cityInput.value = "";
   }
 });
